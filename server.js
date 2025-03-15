8a280f692ad08a4daaf253fdbdc198bb720e253c
AbhishekG-Codes <166795493+AbhishekG-Codes@users.noreply.github.com>
2025-03-15 12:12:17 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+app.use(express.json())
+app.use(cors())
+app.post("/login",(req,res)=>{
+    res.send("success")
+})
+app.listen(3000,(req,res)=>{
+    console.log("server started");
+})
